2667edffee016541398fcac6182bea6dc2b1485f
ChD Xiao <xiaochd@gmail.com>
2024-08-02 14:18:27 +0800

    修改socket读写

--- src/net_main.c
+++ src/net_main.c
@@ -751,10 +751,12 @@ int anetWrite(int fd,const char *buf,int write_len)
             }
         }
 
+        /*
         if(nsended == write_len)
         {
             break;
         }
+        */
 
     } while (1);
     
@@ -792,11 +794,12 @@ int anetRead(int fd,char *buf,int read_len)
             }
         }
 
-        ///if(nreaded == read_len)
+        /*
+        if(nreaded == read_len)
         {
             break;
         }
-
+        */
     } while (1);
     
     return nreaded;
